30955644f4371a6f6a166a000daa2816c648d4c9
Isaac Price <iprice25250@gmail.com>
2023-08-25 16:34:13 -0400

    you can actually run the entire thing from python

--- Controls.py
+++ Controls.py
@@ -2,44 +2,50 @@ import pyautogui
 
 keys = ['up', 'left', 'right', 'down', 'a', 's']
 
+# This class will be used to control mario
 class Input:
     def __init__(self):
         pass
+
+    # This will release all keys
+    def releaseAll(self):
+        for key in keys:
+            pyautogui.keyUp(key)
     
     # This will make mario do nothing
     def none(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
 
     # This will make mario jump
     def jump(self):
-        pyautogui.keyUp(keys)
-        pyautogui.keyDown('up')
+        self.releaseAll()
+        pyautogui.keyDown('s')
 
     # This will make mario walk left
     def left(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
         pyautogui.keyDown('left')
 
     # This will make mario run left
     def runLeft(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
         pyautogui.keyDown('left')
         pyautogui.keyDown('a')
 
     # This will make mario walk right
     def right(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
         pyautogui.keyDown('right')
 
     # This will make mario run right
     def runRight(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
         pyautogui.keyDown('right')
         pyautogui.keyDown('a')
 
     # This will make mario down (crouch if not in the air, or ground pound if in the air)
     def down(self):
-        pyautogui.keyUp(keys)
+        self.releaseAll()
         pyautogui.keyDown('down')
 
     # This will press on the screen where the button for the powerup normally is
