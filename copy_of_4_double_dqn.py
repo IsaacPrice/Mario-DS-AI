63dea4a9294eadad849a0b367867e0de5a0f9cf7
IsaacPrice <iprice25250@gmail.com>
2024-02-23 23:01:14 -0500

    added noisy

--- copy_of_4_double_dqn.py
+++ copy_of_4_double_dqn.py
@@ -30,9 +30,34 @@ import pickle
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 num_gpus = torch.cuda.device_count()
 
+import math
+from torch.nn.init import kaiming_uniform, zeros_
+
+class NoisyLinear(nn.Module):
+  def __init__(self, in_features, out_features, sigma):
+    super(NoisyLinear, self).__init__()
+
+    self.w_mu = nn.Parameter(torch.empty((out_features, in_features)))
+    self.w_sigma = nn.Parameter(torch.empty((out_features, in_features)))
+    self.b_mu = nn.Parameter(torch.empty((out_features)))
+    self.b_sigma = nn.Parameter(torch.empty((out_features)))
+
+    kaiming_uniform(self.w_mu, a=math.sqrt(5))
+    kaiming_uniform(self.w_sigma, a=math.sqrt(5))
+    zeros_(self.b_mu)
+    zeros_(self.b_sigma)
+
+  def forward(self, x, sigma=0.5):
+    if self.training:
+      w_noise = torch.normal(0, sigma, size=self.w_mu.size()).to(device)
+      b_noise = torch.normal(0, sigma, size=self.b_mu.size()).to(device)
+      return F.linear(x, self.w_mu + self.w_sigma * w_noise, self.b_mu + self.b_sigma * b_noise)
+    else:
+      return F.linear(x, self.w_mu, self.b_mu)
+
 class DQN(nn.Module):
 
-  def __init__(self, hidden_size, obs_shape, n_actions): 
+  def __init__(self, hidden_size, obs_shape, n_actions, sigma): 
     super(DQN, self).__init__()
     # Process visual information
     self.conv = nn.Sequential(
@@ -46,14 +71,14 @@ class DQN(nn.Module):
 
     conv_out_size = self._get_conv_out(obs_shape)
     self.head = nn.Sequential(
-        nn.Linear(conv_out_size, hidden_size),
+        NoisyLinear(conv_out_size, hidden_size, sigma=sigma),
         nn.ReLU(),
-        nn.Linear(hidden_size, hidden_size),
+        NoisyLinear(hidden_size, hidden_size, sigma=sigma),
         nn.ReLU(),
     )
 
-    self.fc_adv = nn.Linear(hidden_size, n_actions)
-    self.fc_value = nn.Linear(hidden_size, 1)
+    self.fc_adv = NoisyLinear(hidden_size, n_actions, sigma=sigma)
+    self.fc_value = NoisyLinear(hidden_size, 1, sigma=sigma)
 
   def _get_conv_out(self, shape):
     conv_out = self.conv(torch.zeros(1, *shape))
@@ -67,14 +92,11 @@ class DQN(nn.Module):
     value = self.fc_value(x)
     return value + adv - torch.mean(adv, dim=1, keepdim=True)
 
-def epsilon_greedy(state, env, net, epsilon=0.0):
-  if np.random.random() < epsilon:
-    action = env.action_space.sample()
-  else:
-    state = torch.tensor([state]).to(device)
-    q_values = net(state)
-    _, action = torch.max(q_values, dim=1)
-    action = int(action.item())
+def greedy(state, net):
+  state = torch.tensor([state]).to(device)
+  q_values = net(state)
+  action = q_values.argmax(dim=-1)
+  action = int(action.item())
   return action
 
 class ReplayBuffer:
@@ -140,20 +162,20 @@ def create_environment(name):
 class DeepQLearning(LightningModule):
 
   # Initialize
-  def __init__(self, env_name, policy=epsilon_greedy, capacity=100_000,
+  def __init__(self, env_name, policy=greedy, capacity=100_000,
                batch_size=256, lr=1e-3, hidden_size=128, gamma=0.99,
-               loss_fn=F.smooth_l1_loss, optim=AdamW, eps_start=1.0,
-               eps_end=0.15, eps_last_episode=1000,
+               loss_fn=F.smooth_l1_loss, optim=AdamW,
                samples_per_epoch=10_000, sync_rate=10,
                a_start = 0.5, a_end = 0.0, a_last_episode = 100,
-               b_start = 0.4, b_end = 1.0, b_last_episode = 100):
+               b_start = 0.4, b_end = 1.0, b_last_episode = 100,
+               sigma=0.5):
 
     super().__init__()
     self.env = create_environment(env_name)
 
     obs_shape = self.env.observation_space.shape
     n_actions = self.env.action_space.n
-    self.q_net = DQN(hidden_size, obs_shape, n_actions)
+    self.q_net = DQN(hidden_size, obs_shape, n_actions, sigma=sigma)
     self.target_q_net = copy.deepcopy(self.q_net)
 
     self.policy = policy
@@ -161,31 +183,18 @@ class DeepQLearning(LightningModule):
 
     self.save_hyperparameters()
 
-    if os.path.exists('buffer.pkl'):
-      user_input = input("Do you want to load the initial samples from the file? (y/n): ")
-      if user_input.lower() == 'y':
-        with open('buffer.pkl', 'rb') as f:
-          self.buffer = pickle.load(f)
-      else:
-        self.fill_buffer()
-    else:
-      self.fill_buffer()
-
-  def fill_buffer(self):
     while len(self.buffer) < self.hparams.samples_per_epoch:
       print(f"{len(self.buffer)} samples in experience buffer. Filling.")
-      self.play_episode(epsilon=self.hparams.eps_start)
-    with open('buffer.pkl', 'wb') as f:
-      pickle.dump(self.buffer, f)
+      self.play_episode()
 
   @torch.no_grad()
-  def play_episode(self, policy=None, epsilon=0.):
+  def play_episode(self, policy=None):
     state = self.env.reset()
     done = False
 
     while not done:
       if policy:
-        action = policy(state, self.env, self.q_net, epsilon=epsilon)
+        action = policy(state, self.env, self.q_net)
       else:
         action = self.env.action_space.sample()
 
@@ -212,8 +221,7 @@ class DeepQLearning(LightningModule):
     dataset = RLDataset(self.buffer, self.hparams.samples_per_epoch)
     dataloader = DataLoader(
         dataset=dataset,
-        batch_size=self.hparams.batch_size,
-        num_workers=8
+        batch_size=self.hparams.batch_size
     )
     return dataloader
 
@@ -238,7 +246,7 @@ class DeepQLearning(LightningModule):
     # Compute the priorities and update
     td_errors = (state_action_values - expected_state_action_values).abs().detach()
     for idx, e in zip(indices, td_errors):
-      self.buffer.update(idx, e)
+      self.buffer.update(idx, e.item())
 
     loss = weights * self.hparams.loss_fn(state_action_values, expected_state_action_values, reduction='none')
     loss = loss.mean()
@@ -248,11 +256,6 @@ class DeepQLearning(LightningModule):
 
   # Training epoch end
   def training_epoch_end(self, training_step_outputs):
-    epsilon = max(
-        self.hparams.eps_end,
-        self.hparams.eps_start - self.current_epoch /
-        self.hparams.eps_last_episode
-    )
     alpha = max(
         self.hparams.a_end,
         self.hparams.a_start - self.current_epoch /
@@ -265,9 +268,8 @@ class DeepQLearning(LightningModule):
     )
     self.buffer.alpha = alpha
     self.buffer.beta = beta
-    print(f"Epsilon: {epsilon} ----------------------------------------------")
 
-    self.play_episode(policy=self.policy, epsilon=epsilon)
+    self.play_episode(policy=self.policy)
     self.log('episode/Return', self.env.return_queue[-1])
 
     if self.current_epoch % self.hparams.sync_rate == 0:
@@ -276,12 +278,17 @@ class DeepQLearning(LightningModule):
 
 """#### Train the policy"""
 
-algo = DeepQLearning('LunarLander-v2', hidden_size=512, eps_end=0.05, eps_last_episode=150, sync_rate=3)
+algo = DeepQLearning(
+  'mario', 
+  hidden_size=512,
+  sync_rate=3
+)
 
 trainer = Trainer(
     gpus=num_gpus,
     max_epochs=4_500
 )
+
 
 trainer.fit(algo)
 
